be9617835f4801d0e47b7460877aa37a924c4c19
Sunday Oruwhone <sundayoruwhone@gmail.com>
2022-09-13 07:34:40 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Entry point
- * Description: C program that prints a function followed by a new line
+ * Description: C program that prints text using the 'printf' function
  * Return: 0 if successful
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, ");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 

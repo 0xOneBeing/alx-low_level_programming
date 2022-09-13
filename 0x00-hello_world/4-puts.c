185aa172b4b034ff838a1106dfe068886690e4ec
Sunday Oruwhone <sundayoruwhone@gmail.com>
2022-09-13 07:05:35 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"); /*Escape char for " */
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }

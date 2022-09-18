fe088bf6097d694cea82227efd00d24a8ecf69ff
Sunday Oruwhone <sundayoruwhone@gmail.com>
2022-09-18 05:54:31 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - This program prints from 0 to 9 using putchar
+ * main - print from 0 to 9 using putchar
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i = '0';
+	int num = '0';
 
-	while (i <= '9')
+	while (num <= '9')
 	{
-		putchar(i);
+		putchar(num);
+		num++;
 	}
 	putchar('\n');
 

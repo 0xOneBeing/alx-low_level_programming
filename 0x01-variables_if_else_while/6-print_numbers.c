9578349d8a4ecb639f3d282ec10ef0bb9e0f5c7c
Sunday Oruwhone <sundayoruwhone@gmail.com>
2022-09-18 05:41:15 -0700

    6-print_numbers.c

---
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - This program returns single numbers from 0 - 10 on a new line
+ * Return: return 0 if successful
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}

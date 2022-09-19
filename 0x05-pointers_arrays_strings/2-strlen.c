b9134ae4dee250b16f5890e83b7679b065ccf172
Sunday Oruwhone <sundayoruwhone@gmail.com>
2022-09-19 16:56:11 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,15 +3,17 @@
 /**
  * _strlen - Returns length of a string
  * @s: char variable to hold the string argument
+ *
+ * Return: The length of the string
  */
 
 int _strlen(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; ++i)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		i += i;
+		i++;
 		return i;
 	}
 }

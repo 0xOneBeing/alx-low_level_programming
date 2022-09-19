ecf183c660d408b4cd3e464678586f22b0bce290
Sunday Oruwhone <sundayoruwhone@gmail.com>
2022-09-19 09:06:34 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,7 +4,6 @@
  * swap_int - Swaps thevalues of two int variables with each other
  * @a: integer a
  * @b: integer b
- * @t: integer swapper acting like a temp
  */
 
 void swap_int(int *a, int *b)

29938cf6c831423a561662601c6a67e7fb5d475e
Sunday Oruwhone <sundayoruwhone@gmail.com>
2022-09-21 03:26:52 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - Fxn prints the result of two concatenated string
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+}
+

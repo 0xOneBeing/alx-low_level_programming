8ed77066cd4063f4543bf5265c99452ae01cc53b
Sunday Oruwhone <sundayoruwhone@gmail.com>
2022-09-26 14:46:38 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h";
+
+/**
+ * _memset - Fxn fills memory with a constant byte
+ *
+ * @s: char variable 's'
+ * @b: char varaiabke 'b'
+ * @n: unsigned in variable 'n'
+ *
+ * Return - the pointer to the memory area 's'
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (s);
+}

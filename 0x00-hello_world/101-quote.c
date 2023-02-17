5cb38f1bd797d944c27f8c8757bf22371b312b03
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-02-16 20:15:58 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/^^
+* main - Program start
+* Description: Prints a quote text
+* Return: Returns (1) this time
+*/
+
+int main(void)
+{
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
+}

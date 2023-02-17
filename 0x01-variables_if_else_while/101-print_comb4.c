52c5c833bf1dae7a4ed4587aa20bd8a9ecbdc833
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-02-17 12:41:47 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - start point
+ * Description: Program that prints all possible combo of 3-dogots
+ * Return: (0) safe exit
+ */
+
+int main(void)
+{
+	int i, j, k = '0';
+
+	for (k = '0'; k <= '9'; k++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (i = '0'; i <= '9'; i++)
+			{
+				if (!((i == j) || (k == j) ||
+						(j > i) || (k > j)))
+				{
+					putchar(k);
+					putchar(j);
+					putchar(i);
+					if (!(i == '9' && k == '7' && tens == '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

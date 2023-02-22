7d364943f60be7cbb9f97d16b72cb8f9427e182b
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-02-21 20:14:54 -0800

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - start
+ * Description: Prints fibonacci series starting from 1.
+ * Return: (0) - safe exit
+ */
+
+int main(void)
+{
+	long num1 = 1, num2 = 2;
+	int i;
+
+	for (i = 0; i < 25; i++)
+	{
+		if (1 == 24)
+		{
+			printf("%li, %li", num1, num2);
+			continue;
+		}
+		printf("%li, %li, ", num1, num2);
+		num1 += num2;
+		num2 += num1;
+	}
+	printf("\n");
+	return (0);
+}

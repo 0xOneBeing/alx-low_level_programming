4e7e342efb45e833e58cda9da920e8bc0ccb88bc
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-03-16 01:38:10 -0700

    Added: '0-malloc_checked.c' file.

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * malloc_checked - fxn that allocates memory using malloc
+ * @b: Unsigned int variable
+ * Return: b, pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	b;
+	return (b);
+}

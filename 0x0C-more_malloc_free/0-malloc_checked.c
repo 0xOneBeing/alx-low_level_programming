5d3eb5a32613a2be4467d23e9265144db5ddf609
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-03-16 01:50:11 -0700

    Updated: '0-mallic_checked.c' program file.

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,12 +3,12 @@
 
 /**
  * malloc_checked - fxn that allocates memory using malloc
- * @b: Unsigned int variable
- * Return: b, pointer to the allocated memory
+ * @b: Variable to satisfy sizeof_t
+ * Return: ptr, pointer to the allocated memory block
  */
 
 void *malloc_checked(unsigned int b)
 {
-	b;
-	return (b);
+	int *ptr = malloc(b);
+	return (ptr);
 }

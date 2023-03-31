fb561f98a9df0dff42e6f21397986685f0f834dd
Sunday Obinna Oruwhone <sundayoruwhone@gmail.com>
2023-03-30 22:10:09 -0700

    Added '0-print_list.c' file.

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - Fxn that prints all elements of a list
+ * @h: param list
+ * Return: struct
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t element;
+
+	element = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		element++;
+	}
+	return (element);
+}

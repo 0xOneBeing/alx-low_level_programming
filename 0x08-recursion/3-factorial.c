c6c77350da1882f780ce9be5d6ac5b0378ebc8f4
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-06-09 00:26:01 -0700

    Task 3. Done

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a given number
+ * @n: the number to be 'factorializes'
+ * Return: the factorial
+ */
+
+int factorial(int n)
+{
+	if ((n == 0) || (n == 1))
+		return (1);
+	return ((n) * (factorial(n - 1)));
+}
+/** Code by 0xOneBeing */

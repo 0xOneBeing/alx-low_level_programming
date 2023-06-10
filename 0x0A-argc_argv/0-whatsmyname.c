c394f30a58a8fe8e876de0fc8781ed4d62879d57
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-06-10 14:31:24 -0700

    Task 0. done!

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/** #include <stdlib.h> */
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -17,5 +17,5 @@ int main(int argc, char *argv[])
 
 	printf("%s\n", argv[0]);
 
-	return (0);
+	return (EXIT_SUCCESS);
 }

1453a553a96f4175bcd40cd0aa07a499eff2fec1
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-06-10 14:42:36 -0700

    Task 1. done!

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Prints number of arguments passed into program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: (EXIT_SUCCESS) - safe exit
+ *
+ * Code by 0xOneBeing
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%s\n", argc);
+
+	return (EXIT_SUCCESS);
+}

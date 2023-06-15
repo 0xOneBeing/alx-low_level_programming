354288245a7339bb903b6a539a29b35dc728586b
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-06-14 22:16:54 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+/**
+ * create_size - creates an array of chars
+ * @size: unsigned integer size
+ * @c: character
+ * Return: array of chars
+ *
+ * Code by 0xOneBeing
+ */
+
+char *create_size(unsigned int size, char c)
+{
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	char *array = (char *) malloc(size * sizeof(char));
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+
+	return (array);
+}

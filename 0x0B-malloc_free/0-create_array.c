716cdfb7a190dd68c748d0a7ab3707c166ddeee0
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-06-14 22:27:51 -0700

    Fixed task 0.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_size(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	array = (char *) malloc(size * sizeof(char));
+	array = malloc(size * sizeof(char));
 
 	if (array == NULL)
 		return (NULL);

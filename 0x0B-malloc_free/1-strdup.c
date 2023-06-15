331a5a76fd13c369723d7d2ac51a0f85281bd565
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-06-14 22:48:02 -0700

    Task 1.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - returns pointer to newly allocated space in memeory
+ * @str: the string
+ * Return: duplicate
+ *
+ * Code by 0xOneBeing
+ */
+
+char *_strdup(char *str)
+{
+	size_t length = strlen(str);
+	char *duplicate = (char *) malloc((length + 1) * sizeof(char));
+
+	if ((str == NULL) || (duplicate == NULL))
+		return (NULL);
+
+	strcpy(duplicate, str);
+
+	return (duplicate);
+}

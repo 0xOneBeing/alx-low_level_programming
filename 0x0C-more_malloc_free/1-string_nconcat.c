1c9ff7bdbcb2cf3a39bbd2c74d98bd32808615d5
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-06-15 22:09:09 -0700

    Task 1. again!

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,29 +14,27 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concatenated;
+	unsigned int i, j, k;
+	char *str;
 
-	if (s1 == NULL || s2 == NULL)
-	{
-		s1 = "";
-		s2 = "";
-	}
-
-	size_t len1 = strlen(s1);
-	size_t len2 = strlen(s2);
-
-	if (n >= len2)
-		n = len2;
-
-	concatenated = (char *) malloc((len1 + n + 1) * sizeof(char));
-
-	if (concatenated == NULL)
+	if (s1 == 0 || s2 == 0)
 		return (NULL);
 
-	memcpy(concatenated, s1, len1);
-	memcpy(concatenated + len1, s2, n);
+	if (j > n)
+		j = n;
 
-	concatenated[len1 + n] = '/0';
+	str = malloc((i + j + 1) * sizeof(char));
 
-	return (concatenated);
+	if (str == NULL)
+		return (NULL);
+
+	for (k = 0; k < i; k++)
+		s[k] = s1[k];
+
+	for (k = 0; k < j; k++)
+		s[k + i] = s2[k];
+
+	str[i + j] = '\0';
+
+	return (str);
 }

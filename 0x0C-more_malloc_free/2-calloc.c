2740496e55615625ee823d2886634724e2be94d3
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-06-15 22:19:59 -0700

    Task 2. again!

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,20 +12,19 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *ptr;
 	unsigned int total_size = (nmemb * size), i;
-	unsigned char *byte_ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
+	ptr = malloc(nmemb * size);
+
 	if (ptr == NULL)
 		return (NULL);
 
-	byte_ptr = (unsigned char *) ptr;
-
 	for (i = 0; i < total_size; i++)
-		byte_ptr[i] = 0;
+		ptr[i] = 0;
 
 	return (ptr);
 }

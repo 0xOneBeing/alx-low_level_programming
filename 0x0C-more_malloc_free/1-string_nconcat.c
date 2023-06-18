b5b77bbb4ddc45eb57763e45b0424ed77327d963
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-06-18 10:55:59 -0700

    Task done!

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,11 +17,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, length = n;
 	char *str;
 
-	if (s1 == NULL || s2 == NULL)
-	{
+	if (s1 == NULL)
 		s1 = "";
+
+	if (s2 == NULL)
 		s2 = "";
-	}
 
 	for (i = 0; s1[i]; i++)
 		length++;

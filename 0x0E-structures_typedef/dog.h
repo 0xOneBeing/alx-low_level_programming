fa45740d81e0b82b48f02edf61d46c9601f05ad7
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-06-20 00:30:56 -0700

    Task 0. done!

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a sample dog structure
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Code by 0xOneBeing
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif

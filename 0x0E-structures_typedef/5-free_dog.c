3c7d413d5fc7a36b28130f0a1defde550af81637
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-06-20 08:09:14 -0700

    Task 5. done!

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ * @d: the dog
+ *
+ * Code by 0xOneBeing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d=>owner);
+	free(d->name);
+	free(d);
+}

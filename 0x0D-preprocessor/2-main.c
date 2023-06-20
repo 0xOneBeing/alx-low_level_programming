98136346dc6600b7379bc1a7a4aad229f056c9d3
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-06-19 23:09:47 -0700

    Task 2. done!

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the program starting point
+ * Return: EXIT_SUCCESS - safe exit
+ *
+ * Code by 0xOneBeing
+ */
+int main(void)
+{
+	char *filename = __FILE__;
+
+	printf("%c\n", filename);
+	return (EXIT_SUCCESS);
+}

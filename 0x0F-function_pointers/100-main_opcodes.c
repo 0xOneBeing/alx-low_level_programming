862156d4aa8b73a078f0a108f9da42f76e10e9b8
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-06-22 00:02:44 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- prints opcodes of itself
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: (EXIT_SUCCESS) - safe exit
+ *
+ * Code by 0xOneBeing
+ */
+
+int main(int argc, char argv[])
+{
+	int i, bytes;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[i]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < bytes; i++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (i == bytes - 1)
+			continue;
+		printf(" ");
+
+		address++;
+	}
+
+	printf("\n");
+
+	return (EXIT_SUCCESS);
+}

fd79f9d237f078e9b1a57f3ffe28a0bd130f6c57
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-06-22 14:33:44 -0700

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -13,10 +13,10 @@
 typedef struct printer
 {
 	char *symbol;
-	void (*print)(va_list args);
+	void (*print)(va_list arg);
 } printer_t;
 
-int sum_tree_all(const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
 void print_strings(const char *seperator, const unsigned int n, ...);
 void print_all(const char * const format, ...);

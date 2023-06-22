8d5a02ae500f59d9a28d3106f07a11ffdb1c59fb
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-06-22 15:42:46 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,6 +24,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		if (i < (n - 1) && separator != NULL)
 			printf("%s", separator);
+		else
+			continue;
 	}
 
 	va_end(nums);

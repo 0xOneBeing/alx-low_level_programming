127130551fb4686e09deb78616a6ec813e612eb9
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-07-04 01:56:10 -0700

    4-free_lists.c

---
+++ 0x12-singly_linked_lists/4-free_lists.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - Frees up a mem alloc used for linked list
+ * @head: Pointer to he first node of the list
+ *
+ * Code by 0xOneBeing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}

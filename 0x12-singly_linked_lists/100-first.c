1c477712e27e2c53c97d42859af3c0939bcb1a31
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-07-04 02:02:44 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * bmain - Fxn executed before the main fxn
+ *
+ * Code by 0xOnebeing
+ */
+
+void __attribute__ ((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

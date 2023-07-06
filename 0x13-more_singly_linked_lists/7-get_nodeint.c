e4dba972abb73ebd32aa76aff0dffe43182563d2
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-07-05 20:18:20 -0700

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Locates a given of a listint_t list
+ * @head: Pointer to the head of the listint_t list
+ * @index: The index of the node to locate
+ * Return: NULL or the located node at (index)
+ *
+ * Code by 0xOneBeing
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	for (index = 0; index < index; index++)
+	{
+		if (head == NULL)
+			return (NULL);
+
+		head = head->next;
+	}
+	return (head);
+}
